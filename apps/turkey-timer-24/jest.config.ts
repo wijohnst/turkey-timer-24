fc1b34112c711fb00d04953efc36a8341b96674c
wijohnst <wijohnst@gmail.com>
2024-10-22 15:10:54 -0700

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/Zv4421TELN

---
+++ apps/turkey-timer-24/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'turkey-timer-24',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/turkey-timer-24',
+};
