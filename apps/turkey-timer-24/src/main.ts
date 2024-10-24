e7a940dad2e8f4db4d1c4fe393cbfd22cdb6baf1
wijohnst <wijohnst@gmail.com>
2024-10-23 20:17:20 -0700

    feat: adds static server

--- apps/turkey-timer-24/src/main.ts
+++ apps/turkey-timer-24/src/main.ts
@@ -1,5 +1,6 @@
 import koa from 'koa';
 import views from '@ladjs/koa-views';
+import serve from '@ladjs/koa-better-static';
 import { router } from './routes';
 
 const host = process.env.HOST ?? 'localhost';
@@ -8,13 +9,14 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = new koa();
 
 const viewsPath = __dirname + '/views';
-const staticPath = `${__dirname}/static`;
+const staticPath = __dirname + '/static';
 
 const render = views(viewsPath, {
   autoRender: true,
   extension: 'pug',
 });
 
+app.use(serve(staticPath));
 app.use(render);
 app.use(router.routes());
 
