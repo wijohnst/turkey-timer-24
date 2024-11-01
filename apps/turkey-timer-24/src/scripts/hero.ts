7f84f6b3c76a7e946afdedfab90b1ae8c868c78b
wijohnst <wijohnst@gmail.com>
2024-11-01 14:15:09 -0700

    chore: adds home and transitions

--- apps/turkey-timer-24/src/scripts/hero.ts
+++ apps/turkey-timer-24/src/scripts/hero.ts
@@ -1,33 +1,47 @@
 export const init = () => {
-  console.log('Handling hero section');
-
   document.addEventListener('animationend', (event: AnimationEvent) => {
     const initElements = document.querySelectorAll('.hero-letter.init');
 
+    const classesToRemove = [
+      'init',
+      'glow',
+      'left',
+      'right',
+      'delay-animation',
+      'flicker-on',
+      'top',
+      'right',
+    ];
+
     initElements.forEach((element) => {
-      element.classList.remove('init');
-      element.classList.remove('flicker');
-      element.classList.remove('glow');
-      element.classList.remove('left');
-      element.classList.remove('right');
+      classesToRemove.forEach((classToRemove) => {
+        element.classList.remove(classToRemove);
+      });
     });
 
     const hiddenButton = document.querySelector('button.waffle-button[hidden]');
     if (hiddenButton) {
       hiddenButton.removeAttribute('hidden');
-      hiddenButton.classList.add('flicker');
+      hiddenButton.classList.add('flicker-on');
       hiddenButton.classList.add('glow');
     }
 
     if (event.animationName === 'fadeIn') {
       const overlay = document.getElementById('screen-transition-overlay');
+      const body = document.querySelector('body');
 
       overlay.classList.remove('fade-in');
-      overlay.classList.add('overlay-final');
 
-      window.setInterval(() => {
-        window.htmx.ajax('get', '/home', null);
-      }, 3000);
+      window.history.pushState({ page: 'home' }, 'Home', '/home');
+      window.htmx.ajax('get', '/home', null);
+    }
+
+    if (event.animationName === 'flicker') {
+      const heroLetters = document.querySelectorAll('.hero-letter');
+
+      heroLetters.forEach((heroLetter) => {
+        heroLetter.classList.add('flicker-off');
+      });
     }
   });
 };
