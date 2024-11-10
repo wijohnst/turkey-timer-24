77a6c85dd6304ee36646b322b6237898190f9c5f
wijohnst <wijohnst@gmail.com>
2024-11-10 15:23:27 -0800

    chore: dockerizes app

--- apps/turkey-timer-24/src/main.ts
+++ apps/turkey-timer-24/src/main.ts
@@ -3,7 +3,7 @@ import views from '@ladjs/koa-views';
 import serve from '@ladjs/koa-better-static';
 import { router } from './routes';
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const viewsPath = __dirname + '/views';
@@ -23,5 +23,5 @@ app.use(async (ctx, next) => {
 app.use(router.routes());
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ LET'S GO  ] http://${host}:${port}`);
 });
